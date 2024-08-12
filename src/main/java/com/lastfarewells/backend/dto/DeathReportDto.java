9e9af37991cb61190a0b789aa8e363d0467bb522
atanu2110 <66422706+atanu2110@users.noreply.github.com>
2024-08-12 14:55:52 +0530

    updated death report apis

--- src/main/java/com/lastfarewells/backend/dto/DeathReportDto.java
+++ src/main/java/com/lastfarewells/backend/dto/DeathReportDto.java
@@ -1,5 +1,6 @@
 package com.lastfarewells.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import java.util.Date;
 import lombok.AllArgsConstructor;
@@ -14,6 +15,7 @@ public class DeathReportDto {
     @NotNull(message = "userId cannot be null")
     private Long   userId;
     @NotNull(message = "deathDate cannot be null")
+    @JsonFormat(pattern = "MM/dd/yyyy")
     private Date   deathDate;
     private String attachment;
     private String attachmentFilename;
--- src/main/java/com/lastfarewells/backend/service/impl/ReportDeathServiceImpl.java
+++ src/main/java/com/lastfarewells/backend/service/impl/ReportDeathServiceImpl.java
@@ -4,6 +4,7 @@ import com.lastfarewells.backend.dto.DeathReportDto;
 import com.lastfarewells.backend.entity.DeathReport;
 import com.lastfarewells.backend.repository.DeathReportRepository;
 import com.lastfarewells.backend.service.ReportDeathService;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,8 @@ public class ReportDeathServiceImpl implements ReportDeathService {
         log.info("Reporting death for user {}", deathReportDto.getUserId());
         DeathReport deathReport = DeathReport.builder().userId(deathReportDto.getUserId())
             .deathDate(deathReportDto.getDeathDate()).attachment(deathReportDto.getAttachment())
-            .attachmentFilename(deathReportDto.getAttachmentFilename()).obituaryLink(deathReportDto.getObituaryLink()).build();
+            .attachmentFilename(deathReportDto.getAttachmentFilename()).obituaryLink(deathReportDto.getObituaryLink())
+            .createdOn(Instant.now()).build();
         return deathReportRepository.save(deathReport);
     }
 
