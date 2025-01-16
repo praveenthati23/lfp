f5b9b9f931e35b44195f7696d5a38636326d3c29
praveenthati23 <134906977+praveenthati23@users.noreply.github.com>
2025-01-16 15:39:05 +0530

    Update SqsConfig.java

--- src/main/java/com/lastfarewells/backend/config/SqsConfig.java
+++ src/main/java/com/lastfarewells/backend/config/SqsConfig.java
@@ -1,36 +1,38 @@
 package com.lastfarewells.backend.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
-
-
 @Configuration
 public class SqsConfig {
 
-	@Value("${sqs.accesskey}")
-	private String accessKey;
+    private static final Logger logger = LoggerFactory.getLogger(SqsConfig.class);
 
+    @Value("${sqs.accesskey:dummyAccessKey}") // Default to "dummyAccessKey" if not set
+    private String accessKey;
 
-	@Value("${sqs.secretkey}")
+    @Value("${sqs.secretkey:dummySecretKey}") // Default to "dummySecretKey" if not set
+    private String secretKey;
 
-	private String secretKey;
+    @Value("${sqs.region:us-east-1}") // Default to "us-east-1" if not set
+    private String region;
 
-	@Value("${sqs.region}")
-	private String region;
+    @Bean
+    public SqsAsyncClient sqsAsyncClient() {
+        if ("dummyAccessKey".equals(accessKey) || "dummySecretKey".equals(secretKey)) {
+            logger.warn("AWS SQS credentials are missing. SqsAsyncClient will not be fully operational.");
+        }
 
-
-	@Bean
-	public SqsAsyncClient sqsAsyncClient() {
-		return SqsAsyncClient.builder().region(Region.of(region))
-				.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
-				.build();
-	}
-
+        return SqsAsyncClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
 }
